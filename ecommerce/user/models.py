d50c798a0a74bf038df16f2801f55fd013a1461f
gowri <gowrisnair488@gmail.com>
2026-05-28 16:20:11 +0530

    reg completed

--- ecommerce/user/models.py
+++ ecommerce/user/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=150)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
 
     def __str__(self):
-        return self.username
+        return self.first_name
--- ecommerce/user/views.py
+++ ecommerce/user/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from .models import User
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
 def register(request):
 
-    return render(request, 'user/register.html')
+    if request.method == "POST":
+
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+          
+        )
+        print(User)
+
+    return render(request, 'register.html')
 
 def login_view(request):
+    return HttpResponse("Login Page")
 
-    return render(request, 'user/login.html')
 
 def logout_view(request):
-
-    return render(request, 'user/logout.html')
+    return HttpResponse("Logout Page")
