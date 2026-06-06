129ea9c142699ec83d6cb23aeecc347cd8c03594
gowri <gowrisnair488@gmail.com>
2026-06-06 20:59:18 +0530

    shop-detail cleaning

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class product(models.Model):
     name = models.CharField(max_length=255)
-    brand = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     image = models.URLField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
